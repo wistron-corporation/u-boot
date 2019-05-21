wistron-corporation/u-boot

commit 3c33d1e84ebb8d2305725c509a7c6df1125afa62

aspeed/flash: Add MT25QL01GB chip
The MT25QL01GB is a 128MB NOR flash chip planned to be used on
a Witherspoon system (AST2500).

Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>
Tested-by: Adriana Kobylak <anoo@us.ibm.com>
Signed-off-by: Joel Stanley <joel@jms.id.au>

arch/arm/mach-aspeed/flash.c
@@ -83,6 +83,7 @@ flash_info_t flash_info[CONFIG_SYS_MAX_FLASH_BANKS];		/* FLASH chips info */
 #define SST25VF040B		0x8d25bf
 #define AT25DF161		0x02461F
 #define AT25DF321		0x01471F
+#define MT25QL01GB		0x21ba20
 
 /* SPI Define */
 #define CS0_CTRL			0x10
@@ -1131,6 +1132,19 @@ static ulong flash_get_size (ulong base, flash_info_t *info)
 		case AT25DF321:
 			info->sector_count = 32;
 			info->size = 0x400000;
+			erase_region_size  = 0x10000;
+			info->readcmd = 0x0b;
+			info->dualport = 0;
+			info->dummybyte = 1;
+			info->buffersize = 1;
+			WriteClk = 50;
+			EraseClk = 25;
+			ReadClk  = 50;
+			break;
+
+		case MT25QL01GB:
+			info->sector_count = 2048;
+			info->size = 0x8000000;
 			erase_region_size  = 0x10000;
 			info->readcmd = 0x0b;
 			info->dualport = 0;

commit cc6c5bef00ebbac0b37f9a406913ca17ff9c56b5

aspeed/flash: fix definition of the MT25QL01GB chip
Signed-off-by: Cédric Le Goater <clg@kaod.org>
Fixes: 3c33d1e84ebb ("aspeed/flash: Add MT25QL01GB chip")
Tested-by: Adriana Kobylak <anoo@us.ibm.com>
Signed-off-by: Joel Stanley <joel@jms.id.au>

arch/arm/mach-aspeed/flash.c
@@ -1149,10 +1149,11 @@ static ulong flash_get_size (ulong base, flash_info_t *info)
 			info->readcmd = 0x0b;
 			info->dualport = 0;
 			info->dummybyte = 1;
-			info->buffersize = 1;
+			info->buffersize = 256;
 			WriteClk = 50;
 			EraseClk = 25;
 			ReadClk  = 50;
+			info->address32 = 1;
 			break;
 
 		default:	/* use JEDEC ID */