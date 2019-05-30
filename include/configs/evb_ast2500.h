499853d69fcfcd041222f1b9f7dc754fd93b9ca8
ryan_chen <ryan_chen@aspeedtech.com>
2019-05-30 10:21:04 +0800

    add fmc/spi driver

--- include/configs/evb_ast2500.h
+++ include/configs/evb_ast2500.h
@@ -19,6 +19,9 @@
 /* Memory Info */
 #define CONFIG_SYS_LOAD_ADDR		0x83000000
 
+/* Environment */
 #define CONFIG_ENV_SIZE			0x20000
+#define CONFIG_ENV_OFFSET		0x60000
+#define CONFIG_ENV_SECT_SIZE		(4 << 10)
 
 #endif	/* __CONFIG_H */
