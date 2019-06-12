860cb447b15e305494ca38387271f67c1e5871c8
ryan_chen <ryan_chen@aspeedtech.com>
2019-06-12 13:55:49 +0800

    add ast2600 i2c

--- drivers/i2c/aspeed_i2c_ic.c
+++ drivers/i2c/aspeed_i2c_ic.c
@@ -17,7 +17,7 @@ static int aspeed_i2c_global_probe(struct udevice *dev)
 	struct reset_ctl reset_ctl;
 	int ret = 0;
 
-	debug("%s(dev=%p) =--------\n", __func__, dev);
+	debug("%s(dev=%p) \n", __func__, dev);
 
 	ret = reset_get_by_index(dev, 0, &reset_ctl);
 
@@ -34,6 +34,7 @@ static int aspeed_i2c_global_probe(struct udevice *dev)
 
 static const struct udevice_id aspeed_i2c_global_ids[] = {
 	{ .compatible = "aspeed,ast2500-i2c-ic" },
+	{ .compatible = "aspeed,ast2600-i2c-global" },
 	{ }
 };
 
