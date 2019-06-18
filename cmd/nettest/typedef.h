9df36161bf40da44f900e2c832307f5be7c71ee6
Dylan Hung <dylan_hung@aspeedtech.com>
2019-06-18 16:19:55 +0800

    [new] add nettest command

---
+++ cmd/nettest/typedef.h
@@ -0,0 +1,31 @@
+/*
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _TYPEDEF_H_
+#define _TYPEDEF_H_
+
+#include "swfunc.h"
+
+#define PCI		1
+#define PCIE		2
+#define AGP		3
+
+#define UCHAR		unsigned char
+#define CHAR		char
+#define BYTE		UCHAR
+#define BOOL		SHORT
+#define BOOLEAN		uint16_t
+#define SCHAR		signed char
+
+#define TRUE      	1
+#define FALSE     	0
+
+#endif /* end of "#ifndef _TYPEDEF_H_" */
