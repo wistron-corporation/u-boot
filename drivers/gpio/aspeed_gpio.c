a18292b9f63a144dabb7de7f6e506064033cfa01
ryan_chen <ryan_chen@aspeedtech.com>
2019-07-22 09:21:09 +0800

    update copyright

--- drivers/gpio/aspeed_gpio.c
+++ drivers/gpio/aspeed_gpio.c
@@ -4,10 +4,6 @@
  * Copyright (C) ASPEED Technology Inc.
  * Ryan Chen <ryan_chen@aspeedtech.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
  */
 #include <common.h>
 #include <asm/io.h>
