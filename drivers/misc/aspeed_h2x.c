87c7126e070978146d65c1ef5baa05457bb76e8c
ryan_chen <ryan_chen@aspeedtech.com>
2019-07-29 09:04:34 +0800

    update for pcie

--- drivers/misc/aspeed_h2x.c
+++ drivers/misc/aspeed_h2x.c
@@ -11,6 +11,18 @@
 #include <asm/io.h>
 #include <asm/arch/h2x_ast2600.h>
 
+/* reg 0x80, 0xC0 */
+#define PCIE_RX_TAG_MASK		GENMASK(23, 16)
+#define PCIE_RX_LINEAR			BIT(8)
+#define PCIE_RX_MSI_SEL			BIT(7)
+#define PCIE_RX_MSI_EN			BIT(6)
+#define PCIE_1M_ADDRESS_EN		BIT(5)
+#define PCIE_UNLOCK_RX_BUFF		BIT(4)
+#define PCIE_RX_TLP_TAG_MATCH	BIT(3)
+#define PCIE_Wait_RX_TLP_CLR	BIT(2)
+#define PCIE_RC_L_RX			BIT(1)
+#define PCIE_RC_L				BIT(0)
+
 struct aspeed_h2x_priv {
 	struct aspeed_h2x_reg *h2x;
 };
@@ -19,20 +31,20 @@ static u8 txTag = 0;
 
 extern void aspeed_pcie_cfg_read(struct aspeed_h2x_reg *h2x, u8 type, u32 bdf_offset, ulong *value)
 {
-	u32 timeout =0;
+	u32 timeout = 0;
 
 	txTag %= 0x7;
 	printf("type = %d, busfunc = %x \n",type, bdf_offset);
 	
 	writel(0x04000001 | (type << 24), &h2x->h2x_tx_desc3);
 	writel(0x00002000 | (txTag << 8), &h2x->h2x_tx_desc2);
-	writel(bdf_offset, &h2x->h2x_tx_desc2);
-	writel(0, &h2x->h2x_tx_desc3);
+	writel(bdf_offset, &h2x->h2x_tx_desc1);
+	writel(0x00000000, &h2x->h2x_tx_desc0);
 
 	//trigger tx
 	writel(1, &h2x->h2x_reg24);	
-	//wait 
-//	printf("trigger \n");
+
+	//wait tx idle
 	while(!(readl(&h2x->h2x_reg24) & BIT(31))) {
 		timeout++;
 		if(timeout > 10000) {
@@ -41,6 +53,7 @@ extern void aspeed_pcie_cfg_read(struct aspeed_h2x_reg *h2x, u8 type, u32 bdf_of
 			goto out;
 		}
 	};
+
 	//write clr tx idle
 	writel(1, &h2x->h2x_reg08);
 	
@@ -53,28 +66,37 @@ out:
 
 extern void aspeed_pcie_cfg_write(struct aspeed_h2x_reg *h2x, u8 type, u8 byte_en, u32 bdf_offset, u32 data)
 {
+	u32 timeout = 0;
 	txTag %= 0x7;
 
 	//printf("byte_en : %x, offset: %x, value = %x \n",byte_en , bdf_offset, data);
 
 	writel(0x44000001 | (type << 24), &h2x->h2x_tx_desc3);
 	writel(0x00002000 | (txTag << 8) | byte_en, &h2x->h2x_tx_desc2);
-	writel(bdf_offset, &h2x->h2x_tx_desc2);
-	writel(0, &h2x->h2x_tx_desc3);
+	writel(bdf_offset, &h2x->h2x_tx_desc1);
+	writel(0x00000000, &h2x->h2x_tx_desc0);
 
 	writel(data, &h2x->h2x_tx_data);
 
 	//trigger tx
 	writel(1, &h2x->h2x_reg24);	
-//	printf("trigger \n");	
-	//wait 
-	while(!(readl(&h2x->h2x_reg24) & BIT(31)))
+
+	//wait tx idle
+	while(!(readl(&h2x->h2x_reg24) & BIT(31))) {
+		timeout++;
+		if(timeout > 10000) {
+			printf("time out \n");
+			goto out;
+		}
+	};
 
 	//write clr tx idle
 	writel(1, &h2x->h2x_reg08);
 
+	//clr rx done int
+	writel(0x10, &h2x->h2x_reg88);
+out:
 	txTag++;	
-
 }
 
 static int aspeed_h2x_probe(struct udevice *dev)
@@ -95,9 +117,12 @@ static int aspeed_h2x_probe(struct udevice *dev)
 	writel(0x0, &priv->h2x->h2x_reg64);
 	writel(0xFFFFFFFF, &priv->h2x->h2x_reg68);
 	
-	writel(0x1c7, &priv->h2x->h2x_reg80);
+	writel( PCIE_RX_LINEAR |
+	PCIE_RX_MSI_SEL | PCIE_RX_MSI_EN |
+	PCIE_Wait_RX_TLP_CLR | PCIE_RC_L_RX | PCIE_RC_L,
+	&priv->h2x->h2x_reg80);
 
-	//assign debug tag 
+	//assign debug tx tag
 	writel(0x28, &priv->h2x->h2x_regBC);
 
 	return 0;
