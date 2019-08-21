1b89a6353173619794bfd15b41f115fdc03d19b1
ryan_chen <ryan_chen@aspeedtech.com>
2019-08-21 18:14:15 +0800

    add otp strap bspi size info

--- arch/arm/mach-aspeed/ast2600/aspeed_scu_info.c
+++ arch/arm/mach-aspeed/ast2600/aspeed_scu_info.c
@@ -233,8 +233,15 @@ aspeed_2nd_wdt_mode(void)
 			printf("Single SPI ");
 		else
 			printf("Dual SPI ");
-		printf("= %s \n", readl(0x1e620064) & BIT(4) ? "Alternate":"Primary");
+		printf(": %s", readl(0x1e620064) & BIT(4) ? "Alternate":"Primary");
+
+		if(readl(ASPEED_HW_STRAP2) & GENMASK(15, 13)) {
+			printf(", bspi_size : %ld MB\n", BIT((readl(ASPEED_HW_STRAP2) >> 13) & 0x7));
+		} else
+			printf("\n");
+		
 	}
+	
 }
 
 extern void
