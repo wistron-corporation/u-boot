66d8684b336be84c3cf81d6ae06f58847378b45b
ryan_chen <ryan_chen@aspeedtech.com>
2020-01-15 15:35:15 +0800

    add define for spl build not used

--- drivers/mmc/aspeed_sdhci.c
+++ drivers/mmc/aspeed_sdhci.c
@@ -53,6 +53,7 @@ static int aspeed_sdhci_probe(struct udevice *dev)
 
 	debug("%s: CLK %ld\n", __func__, clock);
 
+#ifndef CONFIG_SPL_BUILD
 	//1: sd card pwr, 0: no pwr
 	gpio_request_by_name_nodev(offset_to_ofnode(node), "pwr-gpios", 0,
 				   &host->pwr_gpio, GPIOD_IS_OUT);
@@ -76,7 +77,7 @@ static int aspeed_sdhci_probe(struct udevice *dev)
 			return ret;
 		}
 	}
-
+#endif
 //	host->quirks = SDHCI_QUIRK_WAIT_SEND_CMD;
 	host->max_clk = clock;
 
